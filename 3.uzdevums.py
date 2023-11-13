514ddcaa50d4b3226aa08327d3e1488271fdf68e
Sanija Sokirka <sanijasokirka@gmail.com>
2023-11-13 10:04:26 +0200

    rety

--- 3.uzdevums.py
+++ 3.uzdevums.py
@@ -1,7 +1,9 @@
-number= str(input("Ievadi skaitli: "))
+number=str(input("Ievadi skaitli: "))
 
-if number %2==0:
-       print(f"Cipars ir pāra skaitlis")
+if  number %2 == 0:
+    print(f"Cipars ir pāra skaitlis")
+elif  number %2 != 0:
+  print(f"Cipars ir nav pāra skaitlis")
 else:
-    print(f"Cipars ir nav pāra skaitlis")
+   print(f"Cipars ir nav pieņemams")
 
